04bdd0ba4302956067e8cd66e2f93edca192b6c2
Serghei Ilin <si@itembase.biz>
2015-10-07 18:30:15 +0200

    Finished FormApi implementation

--- README.md
+++ README.md
@@ -4,6 +4,15 @@ Project contains implementation of the
 [Wufoo API v3](http://help.wufoo.com/articles/en_US/SurveyMonkeyArticleType/Wufoo-REST-API-V3) client in Go.
 Implementation supports JSON response format from Wufoo API.
 
-Current status: under development
+### Current status
+
+Under development
+
+### Currently implemented API's
+
+#### Forms API
+Reference documentation: [here](http://help.wufoo.com/articles/en_US/SurveyMonkeyArticleType/The-Forms-API)
+
+Implemented as a separate struct `FormsApi`.
 
 License: [MIT](https://github.com/itembase/go-wufoo-api-client/blob/master/LICENSE)
--- wufoo/client.go
+++ wufoo/client.go
@@ -10,53 +10,19 @@ import (
 
 // Client represents API client for Wufoo
 type Client struct {
-	config Config
-}
-
-// Creates a new Client object with the provided configuration
-func New(config Config) Client {
-	return &Client{config: config}
-}
-
-// Forms method returns details about all available forms for the API key which was set up in config. It can be used
-// to create a list of all forms and dynamically generate a form embed snippet to use in your application.
-//
-// Method receive the following parameters:
-//
-//     includeTodayCount - will give you today’s entry count for the form.
-//
-// For more details please visit: http://help.wufoo.com/articles/en_US/SurveyMonkeyArticleType/The-Forms-API
-//
-func (c Client) Forms(includeTodayCount bool) {
-	params := make(map[string]string)
-	params["IncludeTodayCount"] = includeTodayCount
-
-	resp, err := http.Get(c.prepareUrl("forms", params))
-}
-
-// FormsDetails method returns details about one form.
-//
-// Method receive the following parameters:
-//     formIdentifier - if not nil Forms will give you information about just one form.
-//                      The call with formIdentifier = nil will return all forms
-//
-//     includeTodayCount - will give you today’s entry count for the form.
-//
-// For more details please visit: http://help.wufoo.com/articles/en_US/SurveyMonkeyArticleType/The-Forms-API
-//
-func (c Client) FormsDetails(formIdentifier string, includeTodayCount bool) (err error) {
-	params := make(map[string]string)
-	params["IncludeTodayCount"] = includeTodayCount
-
-	err = c.get("forms/"+formIdentifier, params, &response)
-	if err != nil {
-		return
-	}
+	Config Config
 }
 
 // get function makes a GET request to wufoo API and returns
-func (c Client) get(api string, params map[string]string, response interface{}) (err error) {
-	resp, err := http.Get(c.prepareUrl(api, params))
+func (c Client) Get(api string, params map[string]string, response interface{}) (err error) {
+	req, err := http.NewRequest("GET", c.PrepareUrl(api, params), nil)
+	if err != nil {
+		return
+	}
+
+	req.SetBasicAuth(c.Config.ApiKey, "footastic")
+
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return
 	}
@@ -76,8 +42,8 @@ func (c Client) get(api string, params map[string]string, response interface{}) 
 }
 
 // prepareUrl function returns URL for specific API with GET parameters if specified
-func (c Client) prepareUrl(api string, params map[string]string) (url string) {
-	url = fmt.Sprintf(WUFOO_API_URL_PATTERN, c.config.Subdomain, api)
+func (c Client) PrepareUrl(api string, params map[string]string) (urlValue string) {
+	urlValue = fmt.Sprintf(WUFOO_API_URL_PATTERN, c.Config.Subdomain, api)
 
 	if params != nil && len(params) > 0 {
 		getParams := url.Values{}
@@ -85,7 +51,7 @@ func (c Client) prepareUrl(api string, params map[string]string) (url string) {
 			getParams.Add(key, value)
 		}
 
-		url += "?" + getParams.Encode()
+		urlValue += "?" + getParams.Encode()
 	}
 
 	return
